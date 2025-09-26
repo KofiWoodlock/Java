97ee612b6e0bcad8016a463a9381e2a53cbbdc92
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-09-26 16:57:37 +0100

    primitive types

---
+++ data types/data_types.java
@@ -0,0 +1,23 @@
+/* Key terms & code
+    - Boolean 
+        - A data type to represent true or false values 
+    - Byte 
+        - 8 bit integer
+    - Char
+        - Character
+    - Double
+        - double precision floating point number (64 bit float)
+    - Float
+        - single precision floating point number 
+    - Int
+        - a positive or negative whole integer
+    - Long 
+        - long integer (supports larger numbers using more bits)
+    - Short
+        - Short intgeger (supports smaller numbers using less bits)
+ */
+
+public class data_types {
+    public static void main(String[] args) {
+    } 
+}
