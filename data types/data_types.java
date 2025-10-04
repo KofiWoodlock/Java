4f079a6485d95a65818da26ccbf88ba6d1daa9de
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-10-04 20:16:49 +0100

    up-to-date

--- data types/data_types.java
+++ data types/data_types.java
@@ -17,9 +17,14 @@
         - Short intgeger (supports smaller numbers using less bits)
  */
 
+import static java.lang.System.out;
+import java.util.Scanner;
+
 public class data_types {
     public static void main(String[] args) {
         chars();
+        booleans();
+        add();
     } 
     public static void inches() {
         long ci;
@@ -27,7 +32,7 @@ public class data_types {
 
         im = 5280 * 12;
         ci = im * im * im;
-        System.out.println("There are " + ci + " cubic inches in cubic mile");
+        out.println("There are " + ci + " cubic inches in cubic mile");
     }
     /* Computes the length of the hypotenuse of a right triangle */
     public static double pythag(double a, double b) {
@@ -39,16 +44,41 @@ public class data_types {
         /* In Java characters are 16-bit unsigned intergral types */
         char a; 
         a = 'A';
-        System.out.println("This is a character: " + a);
+        out.println("This is a character: " + a);
 
         // Char types can be manipulated using arithmetic expressions as they are 
         // just 16 bit intergral types under the hood
-        System.out.println("var a has been incremented: " + ++a);
+        out.println("var a has been incremented: " + ++a);
 
         // Chars can also be assigned integer values which will simply represent 
         // a character in the UNICODE character set 
         a = 67;
-        System.out.println("var a is now: " + a);
+        out.println("var a is now: " + a);
         
+    }
+    public static void booleans() {
+        // The boolean type represents true or false
+        boolean b = false;
+        out.println("The value of b is: " + b);
+        b = true;
+        out.println("The value of b is: " + b);
+        // Booleans can be used in conditionals to control whether some statement is executed 
+        if (b) out.println("This is executed");
+        b = false;
+        if (b) out.println("This is not executed");
+        // A relation operator returns a boolean value
+        out.println("10 > 9 is " + (10 > 9));
+    }
+    public static void add() {
+        int a, b;
+        int sum;
+        try (Scanner input = new Scanner(System.in)) {
+            out.println("Enter a number: ");
+            a = input.nextInt();
+            out.println("Enter another number: "); 
+            b = input.nextInt();
+        }
+        sum = a + b;
+        out.println("result: " + sum);
     }
 }
