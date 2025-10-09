7bd2cf44d91521b298e2b8a2d310de9cda7ab9e1
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-10-09 10:00:47 +0100

    For loops

--- control flow/iteration/for_loops.java
+++ control flow/iteration/for_loops.java
@@ -3,8 +3,18 @@ package iteration;
 public class for_loops {
     public static void main(String[] args) {
         int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        for (int i = 0; i < nums.length; i++)
-            System.err.println(nums[i]);
+        int sum = 0;
+
+        System.out.print("[");
+        for (int i = 0; i < nums.length; i++) {
+            if (i == nums.length - 1)
+                System.out.print(nums[i]);
+            else
+                System.out.print(nums[i] + ",");
+            sum += nums[i];
+        }
+        System.out.println("]");
+        System.out.println("Sum of the array: " + sum);
     }
     
 }
