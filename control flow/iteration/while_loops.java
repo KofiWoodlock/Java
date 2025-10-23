9ac5e244bd02f9326c1390ab297ba6398f3be78a
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-10-23 09:18:28 +0100

    While loop iterations

---
+++ control flow/iteration/while_loops.java
@@ -0,0 +1,12 @@
+package iteration;
+
+public class while_loops {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 5;
+        while (x <= y) {
+            System.out.println(x);
+            x++;
+        }
+    } 
+}
