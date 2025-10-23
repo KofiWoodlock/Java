fd78eb3fd0aabb9bd91cccb76a9d475a2f01d8a3
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-10-23 14:34:40 +0100

    changes 23/20/25

--- control flow/iteration/for_loops.java
+++ control flow/iteration/for_loops.java
@@ -2,7 +2,10 @@ package iteration;
 
 public class for_loops {
     public static void main(String[] args) {
-        int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        nestedLoops();
+        }
+    /* Returns the sum of integer array */
+    public static int arrSum(int nums[]) {
         int sum = 0;
 
         System.out.print("[");
@@ -15,6 +18,30 @@ public class for_loops {
         }
         System.out.println("]");
         System.out.println("Sum of the array: " + sum);
+        return sum;
     }
-    
+    public static void blowup() {
+        for (;;) System.out.println("Nuke the program");
+    }
+    public static void nestedLoops() {
+        int triangle[][] = {{1},
+                           {1, 1},
+                          {1, 2, 1},
+                         {1, 3, 3, 1}
+                    };
+        
+
+        /* Print triangle */ 
+        int rows = triangle.length-1;
+        for (int i = 0; i <= rows; i++) {
+
+            for (int j = rows; j >= i; j--) {
+                System.out.print(" ");
+            } 
+            for (int j = 0; j <= i; j++) {
+                System.out.print(triangle[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
