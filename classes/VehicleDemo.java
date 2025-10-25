ed2f28d8e11361b0324981669cb3fc460973b57c
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-10-25 20:23:31 +0100

    changes 25/10/25

--- classes/VehicleDemo.java
+++ classes/VehicleDemo.java
@@ -1,7 +1,17 @@
 package classes;
 
 public class VehicleDemo {
-    
+    public static void main(String args[]) {
+        /* To create an object we need to instanciate the class */
+        Vehicle car = new Vehicle();
+        /* To access instance variables we use object.member */
+        car.fuelcap = 16;
+        car.passengers = 5;
+        car.mpg = 33;
+        /* Calculate the distance that can be travelled on a full tank */
+        int d = car.fuelcap * car.mpg;
+        System.out.println("Car has " + car.passengers + " passengers. With a max distance of miles" + d);
+    } 
 }
 
 // A class creates a new data type
