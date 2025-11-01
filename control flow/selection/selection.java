a7ee8f28a528fb6079c558d96d38571fb51b89f3
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-11-01 16:49:56 +0000

    control flow - selection

--- control flow/selection/selection.java
+++ control flow/selection/selection.java
@@ -3,10 +3,12 @@ package selection;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class selection {
     public static void main(String[] args) throws IOException {
-        game();        
+        // game();        
+        switches();
     }
     /* Letter guessing game */
     public static void game() throws IOException{        
@@ -27,10 +29,55 @@ public class selection {
                     System.out.println("Correct!"); 
                     notGuessed = false;
                 }
-                else System.out.println("Incorrect!");
+                else  {
+                    System.out.println("Incorrect!");
+                    if (ch < answer) System.out.println("You're guess is too low");
+                    else System.out.println("You're guess is too high");
+                }
             }
         } catch (IOException e) {
             System.out.println("Error!" + e);
+        }
+    }
+    /* Method to demonstrate switch statements */
+    public static void switches() {
+        int number;
+
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Enter a number");
+            number = in.nextInt();
+        }
+
+        switch (number) {
+            case 1:
+                System.out.println("Number is 1");
+                break;
+            case 2:
+                System.out.println("Number is 2");
+                break;
+            case 3:
+                System.out.println("Number is 3");
+                break;
+            default:
+                System.out.println("Number is not between 1-3");
+        }
+        /* Nested switches */
+        char ch1 = 'A';
+        char ch2 = 'B';
+
+        switch (ch1) {
+            case 'A': System.out.println("outer ch1 is A");
+                switch (ch2) {
+                    case 'A':
+                        System.out.println("inner ch2 is A");
+                        break;
+                    case 'B':
+                        System.out.println("inner ch2 is B");
+                }
+                break;
+            case 'B':
+                System.out.println("outer ch1 is B");
+                break;
         }
     }
 }
