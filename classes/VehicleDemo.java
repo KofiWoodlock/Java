4c843e3c25202633ab61bcb7c376c939982fb276
Kofi Woodlock <kofiwoodlock@gmail.com>
2025-12-15 13:22:34 +0000

    Constructors

--- classes/VehicleDemo.java
+++ classes/VehicleDemo.java
@@ -7,22 +7,34 @@ package classes;
     - Links an object with its member (object.member)
  * new operator
     - Dynamically (during runtime) allocates memory for an object & returns a reference to it
+ * constructor
+    - Initialises the instance members of an object when it is instaciated
+ * defualt contstructor
+    - An implicit constructor that sets all instance variables to their default values
+    or 0, null and false for numeric types, reference types and booleans if no value has been set
+ * parameterized constructor
+    - A constructor that takes a list of arguments during instanciation
+ * Garbage collection
+    - A system that recalims dynamically allocated memory automatically when there are no references to
+    an object
+ * finalize()
+    - A method called before an object is destroyed to ensure the object is destroyed cleanly
+ * protected
+    - An access specifier that limits access to the member or method only to members of the same class
+    or subclasses
  */
 
 public class VehicleDemo {
     public static void main(String args[]) {
         /* To create an object we need to instanciate the class */
-        Vehicle car = new Vehicle();
+        Vehicle car = new Vehicle(5, 16, 33);
         /* the variable car is a reference to the memory location of the instance of the object Vehicle */
         /* To access instance variables we use object.member */
-        car.fuelcap = 16;
-        car.passengers = 5;
-        car.mpg = 33;
         /* Calculate the distance that can be travelled on a full tank */ 
         System.out.println("Car has " + car.passengers + " passengers and has a range of " + car.range() + " miles");
 
         /* References work differently to primitive types during assignment */
-        Vehicle car1 = new Vehicle();
+        Vehicle car1 = new Vehicle(0, 0, 0);
         Vehicle car2 = car1; /* car2 and car1 refer to the same object */
         /* the object can be acted upon by both car1 and car2 for example */
         car1.mpg = 26;
@@ -37,6 +49,12 @@ class Vehicle {
     int passengers; // Number of seats
     int fuelcap; // fuel capacity 
     int mpg; // fuel consumption
+
+    Vehicle(int passenger, int fuel, int mpg) {
+        this.passengers = passenger;
+        this.fuelcap = fuel;
+        this.mpg = mpg;
+    }
 
     /* Displays the range of the vehicle */
     int range() {
